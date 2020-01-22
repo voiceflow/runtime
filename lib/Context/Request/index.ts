4b055ccda65b96043725b4730c6bb04d997a7905
Tyler Han <tylerhan97@gmail.com>
2020-01-22 10:56:56 -0500

    wip: move things out to alexa
    wip: immutable request

--- lib/Context/Request/index.ts
+++ lib/Context/Request/index.ts
@@ -1,14 +1,4 @@
-import Store from '../Store';
-
-export type Mapping = { variable: string; slot: string };
-
-export type Choice = {
-  mappings: Array<Mapping>;
-  intent: string;
-  nextIdIndex?: number;
-};
-
 export default interface Request {
   type: string;
-  payload: Store;
+  payload: Record<string, any>;
 }
