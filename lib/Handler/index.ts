969644d64e9eb6c6dfa049adc42216dae9d31da8
leartgjoni-voiceflow <leart@voiceflow.com>
2020-01-22 09:57:53 -0500

    feat: add set handler
    feat: add actual express eval
    
    feat: added mathjs eval thread
    
    wip: default mathjs methods
    
    refactor: cleanup

--- lib/Handler/index.ts
+++ lib/Handler/index.ts
@@ -8,6 +8,7 @@ import EndHandler from './end';
 import FlowHandler from './flow';
 import StartHandler from './start';
 import RandomHandler from './random';
+import SetHandler from './set';
 
 type Block = Record<string, any>;
 
@@ -16,6 +17,6 @@ export interface Handler {
   handle: (block: Block, context: Context, variables: Store, diagram: Diagram) => string | Promise<string>;
 }
 
-export const DefaultHandlers = [CodeHandler, EndHandler, FlowHandler, StartHandler, RandomHandler];
+export const DefaultHandlers = [CodeHandler, EndHandler, FlowHandler, StartHandler, RandomHandler, SetHandler];
 
 export default Handler;
