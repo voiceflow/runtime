b309b8b7200bbb412088dbf165c1ae33e66b4d29
Evgeny <overhawlin@gmail.com>
2020-02-14 10:40:45 +0300

    wip: export event callback type

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ export { default as Request } from '@/lib/Context/Request';
 export { default as Context, State } from '@/lib/Context';
 export { default as Handler, Block } from '@/lib/Handler';
 export { default as Diagram, Command } from '@/lib/Diagram';
-export { Event } from '@/lib/Lifecycle';
+export { Event, Callback as EventCallback } from '@/lib/Lifecycle';
 export * from '@/lib/Util';
 
 export { default } from '@/lib/Client';
