89e360c4d768cae9bb328293bf0b5b15b54b0451
Evgeny Zaytsev <overhawlin@gmail.com>
2020-03-31 20:22:35 +0300

    feat: export default handlers (#43)

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ export { default as Stack } from '@/lib/Context/Stack';
 export { default as Store } from '@/lib/Context/Store';
 export { default as Request } from '@/lib/Context/Request';
 export { default as Context, State } from '@/lib/Context';
-export { default as Handler, Block } from '@/lib/Handler';
+export { default as Handler, Block, DefaultHandlers } from '@/lib/Handler';
 export { default as Diagram, Command } from '@/lib/Diagram';
 export { EventType, Event, EventCallback, CallbackEvent } from '@/lib/Lifecycle';
 export * from '@/lib/Constants';
