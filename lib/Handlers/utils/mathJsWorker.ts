f15ca051615ef1f89b19ebfea1b3e657268c7d5a
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2020-04-15 10:58:57 -0400

    fix: override math ops with custom methods (#45)

--- lib/Handlers/utils/mathJsWorker.ts
+++ lib/Handlers/utils/mathJsWorker.ts
@@ -26,6 +26,30 @@ math.import!(
       throw new Error('Function derivative is disabled');
     },
     // end of security fix
+
+    // override native methods
+    add: (x: any, y: any) => x + y,
+    subtract: (x: any, y: any) => x - y,
+    multiply: (x: any, y: any) => x * y,
+    divide: (x: any, y: any) => x / y,
+
+    equal: (x: any, y: any) => x == y, // eslint-disable-line eqeqeq
+    or: (x: any, y: any) => x || y,
+    and: (x: any, y: any) => x && y,
+    not: (x: any) => !x,
+
+    pow: (x: any, y: any) => x ** y,
+    compare: (x: any, y: any) => {
+      // eslint-disable-next-line eqeqeq
+      if (x == y) {
+        return 0;
+      }
+      if (x > y) {
+        return 1;
+      }
+
+      return -1;
+    },
   },
   { override: true }
 );
