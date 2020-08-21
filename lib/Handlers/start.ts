14769f83144a703136ec7faf21cd80adad766d41
Tyler Han <tylerhan97@gmail.com>
2020-08-21 03:09:25 -0700

    fix: accept start type

--- lib/Handlers/start.ts
+++ lib/Handlers/start.ts
@@ -1,12 +1,13 @@
 import { HandlerFactory } from '@/lib/Handler';
 
 export type Start = {
+  type?: string;
   nextId?: string;
 };
 
 const StartHandler: HandlerFactory<Start> = () => ({
   canHandle: (block) => {
-    return Object.keys(block).length === 2 && !!block.nextId;
+    return (Object.keys(block).length === 2 || block.type === 'start') && !!block.nextId;
   },
   handle: (block, context) => {
     context.trace.debug('beginning flow');
