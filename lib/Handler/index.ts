353a136511a9e2a2a38555a622894da57bea4808
Tyler Han <tylerhan97@gmail.com>
2020-09-02 20:21:43 -0400

    fix: lenient handler type

--- lib/Handler/index.ts
+++ lib/Handler/index.ts
@@ -4,7 +4,7 @@ import Context from '@/lib/Context';
 import Store from '@/lib/Context/Store';
 import Program from '@/lib/Program';
 
-export default interface Handler<N extends Node = Node> {
+export default interface Handler<N extends Node = Node<any, any>> {
   canHandle: (node: N, context: Context, variables: Store, program: Program) => boolean;
   handle: (node: N, context: Context, variables: Store, program: Program) => null | string | Promise<string | null>;
 }
