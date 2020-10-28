9368a947bf103b6aac6c139b4cfd2ef239a0ba5b
Evgeny <overhawlin@gmail.com>
2020-10-28 11:27:30 +0300

    fix: add generics to create context

--- lib/Client/index.ts
+++ lib/Client/index.ts
@@ -1,11 +1,12 @@
 import Context, { Options as ContextOptions, State as ContextState } from '@/lib/Context';
 import Request from '@/lib/Context/Request';
+import { DataAPI } from '@/lib/DataAPI';
 import { AbstractLifecycle } from '@/lib/Lifecycle';
 
-class Controller extends AbstractLifecycle {
-  private options: Pick<ContextOptions, 'api' | 'handlers' | 'services'>;
+class Controller<D extends DataAPI = DataAPI> extends AbstractLifecycle {
+  private options: Pick<ContextOptions<D>, 'api' | 'handlers' | 'services'>;
 
-  constructor({ api, handlers = [], services = {} }: ContextOptions) {
+  constructor({ api, handlers = [], services = {} }: ContextOptions<D>) {
     super();
 
     this.options = {
@@ -15,8 +16,8 @@ class Controller extends AbstractLifecycle {
     };
   }
 
-  public createContext(versionID: string, state: ContextState, request?: Request, options?: ContextOptions): Context {
-    return new Context(versionID, state, request, { ...this.options, ...options }, this.events);
+  public createContext(versionID: string, state: ContextState, request?: Request, options?: ContextOptions<D>): Context<D> {
+    return new Context<D>(versionID, state, request, { ...this.options, ...options }, this.events);
   }
 }
 
