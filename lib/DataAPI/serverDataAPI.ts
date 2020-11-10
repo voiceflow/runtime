0a2db8b89a7a8fc39c43f45ab323e43aa16c140b
leartgjoni-voiceflow <leart@voiceflow.com>
2020-11-10 15:26:26 -0500

    feat: server data api token auto gen

--- lib/DataAPI/serverDataAPI.ts
+++ lib/DataAPI/serverDataAPI.ts
@@ -4,14 +4,37 @@ import { AxiosInstance, AxiosStatic } from 'axios';
 import { DataAPI, Display } from './types';
 
 class ServerDataAPI<P extends Program<any, any>, V extends Version<any>> implements DataAPI<P, V> {
-  private client: AxiosInstance;
+  private client!: AxiosInstance;
 
-  constructor({ dataSecret, dataEndpoint }: { dataSecret: string; dataEndpoint: string }, { axios }: { axios: AxiosStatic }) {
-    this.client = axios.create({
-      baseURL: dataEndpoint,
-      headers: { authorization: `Bearer ${dataSecret}` },
-    });
+  private axios: AxiosStatic;
+
+  private dataEndpoint: string;
+
+  private adminToken: string;
+
+  constructor({ adminToken, dataEndpoint }: { adminToken: string; dataEndpoint: string }, { axios }: { axios: AxiosStatic }) {
+    this.axios = axios;
+    this.dataEndpoint = dataEndpoint;
+    this.adminToken = adminToken;
   }
+
+  public init = async () => {
+    const {
+      data: { token: VF_DATA_SECRET },
+    } = await this.axios.post(
+      `${this.dataEndpoint}/generate-platform-token`,
+      {
+        platform: 'alexa',
+        ttl_min: 525600,
+      },
+      { headers: { admintoken: this.adminToken } }
+    );
+
+    this.client = this.axios.create({
+      baseURL: this.dataEndpoint,
+      headers: { authorization: `Bearer ${VF_DATA_SECRET}` },
+    });
+  };
 
   public fetchDisplayById = async (displayId: number): Promise<null | Display> => {
     const { data }: { data: undefined | null | Display } = await this.client.get(`/metadata/displays/${displayId}`);
--- tests/lib/Client/serverDataApi.unit.ts
+++ tests/lib/Client/serverDataApi.unit.ts
@@ -3,19 +3,47 @@ import sinon from 'sinon';
 
 import ServerDataAPI from '@/lib/DataAPI/serverDataAPI';
 
+const getServerDataApi = async (axiosInstance: Record<string, Function>) => {
+  const axios = { create: sinon.stub().returns(axiosInstance), post: sinon.stub().returns({ data: { token: 'secret-token' } }) };
+  const testConfig = { dataEndpoint: 'data-endpoint', adminToken: 'admin-token' };
+
+  const client = new ServerDataAPI(testConfig, { axios } as any);
+  await client.init();
+
+  return client;
+};
+
 describe('serverDataAPI client unit tests', () => {
   describe('new', () => {
-    it('works correctly', () => {
-      const axios = { create: sinon.stub() };
+    it('works correctly', async () => {
+      const dataSecret = 'secret-token';
+      const adminToken = 'admin-token';
+      const dataEndpoint = 'random';
 
-      // eslint-disable-next-line no-new
-      new ServerDataAPI({ dataSecret: 'test', dataEndpoint: 'random' }, { axios } as any);
+      const axios = { post: sinon.stub().returns({ data: { token: dataSecret } }), create: sinon.stub() };
+      const testConfig = {
+        adminToken,
+        dataEndpoint,
+      };
 
+      const serverDataAPI = new ServerDataAPI(testConfig, { axios } as any);
+      await serverDataAPI.init();
+
+      expect(axios.post.args).to.eql([
+        [
+          `${dataEndpoint}/generate-platform-token`,
+          {
+            platform: 'alexa',
+            ttl_min: 525600,
+          },
+          { headers: { admintoken: adminToken } },
+        ],
+      ]);
       expect(axios.create.args).to.eql([
         [
           {
-            baseURL: 'random',
-            headers: { authorization: 'Bearer test' },
+            baseURL: testConfig.dataEndpoint,
+            headers: { authorization: `Bearer ${dataSecret}` },
           },
         ],
       ]);
@@ -24,25 +52,51 @@ describe('serverDataAPI client unit tests', () => {
 
   describe('fetchDisplayById', () => {
     it('no data', async () => {
-      const axiosInstance = { get: sinon.stub().returns({}) };
-      const axios = { create: sinon.stub().returns(axiosInstance) };
-
-      const client = new ServerDataAPI({ dataSecret: 'test', dataEndpoint: 'random' }, { axios } as any);
+      const axios = { get: sinon.stub().returns({}) };
+      const client = await getServerDataApi(axios);
 
       const displayId = 1;
       expect(await client.fetchDisplayById(displayId)).to.eql(null);
-      expect(axiosInstance.get.args).to.eql([[`/metadata/displays/${displayId}`]]);
+      expect(axios.get.args).to.eql([[`/metadata/displays/${displayId}`]]);
     });
 
     it('with data', async () => {
       const data = { foo: 'bar' };
-      const axiosInstance = { get: sinon.stub().returns({ data }) };
-      const axios = { create: sinon.stub().returns(axiosInstance) };
-
-      const client = new ServerDataAPI({ dataSecret: 'test', dataEndpoint: 'random' }, { axios } as any);
+      const axios = { get: sinon.stub().returns({ data }) };
+      const client = await getServerDataApi(axios);
 
       const displayId = 1;
       expect(await client.fetchDisplayById(displayId)).to.eql(data);
     });
+  });
+
+  it('getProgram', async () => {
+    const data = { foo: 'bar' };
+    const axios = { get: sinon.stub().returns({ data }) };
+    const client = await getServerDataApi(axios);
+
+    const programId = '1';
+    expect(await client.getProgram(programId)).to.eql(data);
+    expect(axios.get.args).to.eql([[`/diagrams/${programId}`]]);
+  });
+
+  it('getTestProgram', async () => {
+    const data = { foo: 'bar' };
+    const axios = { get: sinon.stub().returns({ data }) };
+    const client = await getServerDataApi(axios);
+
+    const programId = '1';
+    expect(await client.getTestProgram(programId)).to.eql(data);
+    expect(axios.get.args).to.eql([[`/test/diagrams/${programId}`]]);
+  });
+
+  it('getVersion', async () => {
+    const data = { foo: 'bar' };
+    const axios = { get: sinon.stub().returns({ data }) };
+    const client = await getServerDataApi(axios);
+
+    const versionId = '1';
+    expect(await client.getVersion(versionId)).to.eql(data);
+    expect(axios.get.args).to.eql([[`/version/${versionId}`]]);
   });
 });
