32452a5a55fc1766f4bf99748580c1a29e2edf1b
Evgeny Zaytsev <overhawlin@gmail.com>
2021-01-05 18:52:27 +0300

    feat: add get project endpoint (#74)
    * feat: add get project endpoint
    
    * feat: add local project

--- lib/DataAPI/localDataAPI.ts
+++ lib/DataAPI/localDataAPI.ts
@@ -1,11 +1,14 @@
-import { Program, Version } from '@voiceflow/api-sdk';
+import { BasePlatformData, Program, Project, Version } from '@voiceflow/api-sdk';
 import * as FS from 'fs';
 import * as Path from 'path';
 
 import { DataAPI } from './types';
 
-class LocalDataAPI<P extends Program<any, any>, V extends Version<any>> implements DataAPI<P, V> {
+class LocalDataAPI<P extends Program<any, any>, V extends Version<any>, PJ extends Project<any, any> = Project<BasePlatformData, BasePlatformData>>
+  implements DataAPI<P, V, PJ> {
   private version: V;
+
+  private project: PJ;
 
   private programs: Record<string, P>;
 
@@ -15,6 +18,7 @@ class LocalDataAPI<P extends Program<any, any>, V extends Version<any>> implemen
     const content = JSON.parse(fs.readFileSync(path.join('projects', projectSource), 'utf8'));
 
     this.version = content.version;
+    this.project = content.project;
     this.programs = content.programs;
   }
 
@@ -25,6 +29,8 @@ class LocalDataAPI<P extends Program<any, any>, V extends Version<any>> implemen
   public getVersion = async () => this.version;
 
   public getProgram = async (programID: string) => this.programs[programID];
+
+  public getProject = async () => this.project;
 
   public fetchDisplayById = async () => null;
 }
--- lib/DataAPI/types.ts
+++ lib/DataAPI/types.ts
@@ -1,10 +1,11 @@
-import { Command, Node, Program, Version, VersionPlatformData } from '@voiceflow/api-sdk';
+import { BasePlatformData, Command, Node, Program, Project, Version, VersionPlatformData } from '@voiceflow/api-sdk';
 
 export type Display = { document?: string };
 
 export interface DataAPI<
   P extends Program<Node, Command> = Program<Node, Command>,
-  V extends Version<VersionPlatformData> = Version<VersionPlatformData>
+  V extends Version<VersionPlatformData> = Version<VersionPlatformData>,
+  PJ extends Project<any, any> = Project<BasePlatformData, BasePlatformData>
 > {
   init(): Promise<void>;
 
@@ -13,4 +14,6 @@ export interface DataAPI<
   getProgram(programID: string): Promise<P>;
 
   getVersion(versionID: string): Promise<V>;
+
+  getProject(projectID: string): Promise<PJ>;
 }
